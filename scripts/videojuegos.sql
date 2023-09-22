db3b6c6ca8a22f7b848941935d4e72601232204f
EddWlad <tidsec_inf@outlook.com>
2023-09-22 09:53:49 -0500

    correccion de la tabla videojuegos

--- scripts/videojuegos.sql
+++ scripts/videojuegos.sql
@@ -1,9 +1,10 @@
 --creacion de tabla videojuegos
+drop table videojuegos
 create table videojuegos
 (
 	codigo int not null,
 	nombre varchar(100) not null,
 	descripcion varchar(300),
-	valoracion int,
+	valoracion int not null,
 	constraint videojuegos_pk primary key(codigo)
 )
